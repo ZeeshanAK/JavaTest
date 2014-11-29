69b7c3a79224f8754dfad186999b52403eb523a0
ZeeshanAK <zeeshankhan.a1@gmail.com>
2014-11-29 22:21:52 +0500

    Adding DoWhile statment

---
+++ ControlStatements/src/DoWhileStatement.java
@@ -0,0 +1,16 @@
+
+public class DoWhileStatement {
+
+	public static void main(String[] args) 
+	{
+		int i = 0; 
+		do
+		{
+			System.out.printf("I am line number %d%n", i);
+			i++;
+		}
+		while (i <= 10);
+			
+	}
+
+}
