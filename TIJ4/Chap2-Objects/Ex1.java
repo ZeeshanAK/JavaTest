5d5aacc44ab4030b08f3fa82aa5e45ebdfd97975
ZeeshanAK <zeeshankhan.a1@gmail.com>
2015-02-08 17:33:42 +0500

    Adding TIJ4

---
+++ TIJ4/Chap2-Objects/Ex1.java
@@ -0,0 +1,15 @@
+/*
+ * Exercise 1: (2) Create a class containing an int and a char that are not initialized, 
+ * and print their values to verify that Java performs default initialization.
+ */
+public class Ex1 {
+	static int 	x;
+	static char c;
+
+	public static void main(String[] args) {
+				
+		System.out.println(x);
+		System.out.println(c);
+	}
+
+}
