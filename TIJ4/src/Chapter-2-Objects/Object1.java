43f2dfa1e2f4a3266b16e0f87ef6fa4f24d09172
ZeeshanAK <zeeshankhan.a1@gmail.com>
2015-02-08 16:53:09 +0500

    Did nothing

--- TIJ4/src/Chapter-2-Objects/Object1.java
+++ TIJ4/src/Chapter-2-Objects/Object1.java
@@ -2,7 +2,6 @@
  * Exercise 1: (2) Create a class containing an int and a char that are not initialized, 
  * and print their values to verify that Java performs default initialization.
  */
-
 public class Object1 {
 	static int 	x;
 	static char c;
